10ff248ff981fa80da1f17ae4553564bb6fbfa8a
losif63 <jaduksuh@naver.com>
2023-01-09 15:40:13 +0900

    Began writing first source code

---
+++ src/dlsim/tensor_4d.cc
@@ -0,0 +1,29 @@
+#include "dlsim/tensor_4d.h"
+
+namespace dlsim 
+{
+
+template <class T>
+Tensor4d<T>::Tensor4d() {
+
+}
+
+template <class T>
+Tensor4d<T>::Tensor4d(
+    unsigned    ndim,
+    unsigned    N,
+    unsigned    C,
+    unsigned    H,
+    unsigned    W,
+    char*       type,
+    int         sparsity
+) {
+
+}
+
+
+
+
+}
+
+;
